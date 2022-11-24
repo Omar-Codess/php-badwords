a2b84e563adec62b28a78691889863530f14455d
Omar-Codess <omartaki8@gmail.com>
2022-11-24 17:51:10 +0100

    aggiunta funzione che sostituisce badword con asterischi

--- censor.php
+++ censor.php
@@ -1,6 +1,9 @@
 <?php
 var_dump($_GET);
+
 $paragraph = $_GET["paragraph"];
+$badword = $_GET["badword"];
 echo $paragraph;
-echo strlen ("paragraph");
+echo strlen ($paragraph);
+echo str_replace ($badword,"***",$paragraph);
 ?>
